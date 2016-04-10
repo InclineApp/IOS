3b3c1ef9b8334b926ba2f5ad6ad39895d15ddc3e
jakeschroeder001 <jakeschroeder8@gmail.com>
2016-04-09 19:56:08 -0500

    add space

--- InclineApp/Main.cs
+++ InclineApp/Main.cs
@@ -7,7 +7,7 @@ namespace InclineApp
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
+            // if you want to use a different Application De legate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
